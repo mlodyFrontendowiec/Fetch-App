b9713d0f6a0c2fe511420e04bfd8c0592c3a4a62
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-15 21:40:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const getUsers = (e) => {
+  e.preventDefault();
+
+  const usersNumber = document.querySelector('[name = "users-number"]').value;
+  const userGender = document.querySelector('[name = "gender"]').value;
+  console.log(usersNumber, userGender);
+  const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${
+    userGender === "both" ? "male,female" : userGender
+  }`;
+  console.log(url);
+  fetch(url)
+    .then((res) => {
+      if (res.status !== 200) {
+        throw Error("To nie jest odpowiedź 200");
+      } else {
+        return res.json();
+      }
+    })
+    .then((json) => showUsers(json.results))
+    .catch((err) => console.log(err));
+};
+const showUsers = (users) => {
+  const resultArea = document.querySelector(".user-list");
+  resultArea.textContent = "";
+  users.forEach((user) => {
+    const item = document.createElement("div");
+    item.className = "user";
+    item.innerHTML = `
+    <div class="user__name">${user.name.title.toUpperCase()} ${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}</div>
+    <img class="user__image" src=${user.picture.medium}>
+    `;
+    resultArea.appendChild(item);
+  });
+};
+
+document.querySelector(".generator").addEventListener("submit", getUsers);
